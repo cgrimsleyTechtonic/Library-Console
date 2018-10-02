ef0f2762f68d1e50c7a9d4103c94039e55fb90e0
Cody Grimsley <cody.grimsley@techtonic.com>
2018-10-02 15:36:03 -0600

    got addBook logic working, spent 1.5 hours trying to figure out how to access the bookshelf array..... glibrary, not library.

--- js/engine.js
+++ js/engine.js
@@ -4,19 +4,23 @@ function Library(){
 
 
 
-// prob should use Library.bookShelf instead of Library.
+
 Library.prototype.addBook = function (book){
   var bookBool;
   function bookChk(b){
     return b === book;
   }
-
-  bookBool = Library.filter(bookChk);
-  console.log(bookBool);
-  if (bookBool){
-    Library.push(book);
-    return bookBool;
-  } else {return false;}
+  bookBool = gLibrary.bookShelf.find(bookChk);
+  //console.log(bookBool);
+  console.log("bookchck started");
+  if (!bookBool){
+    gLibrary.bookShelf.push(book);
+    console.log("bookchk true, book pushed");
+    return true;
+  } else {
+    console.log("bookchk false, book not pushed");
+    return false;
+  }
 };
 
 Library.prototype.removeBookByTitle = function (title){

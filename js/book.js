9fb4b128934ae42cfe3cd952e11893b5cabb2b44
Cody Grimsley <cody.grimsley@techtonic.com>
2018-10-03 10:18:31 -0600

    Sufficient amount of hair removed. Got remove book by title logic working as defined in the doc, same for remove book by author. Looking for ways to implement DRY, i feel there are some reusability options available to me but that is something to circle back on. Began investing in tiger blood product, now winning.

--- js/book.js
+++ js/book.js
@@ -2,10 +2,16 @@ function Book(title, author, numPages, pubDate){
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.pubData = pubDate;
+  this.pubDate = pubDate;
 };
 
 Book.prototype.editBook = function(oBook){
 
 };
+
+
+// vvvvvvvv preloaded book for testing vvvvvvv
+var fakebook1 = new Book("fakebook1", "fakeAuth1", 2, "fakeDate1");
 var ringworld = new Book("RingWorld", "Larry Niven", 342, "October 1980");
+var fakebook2 = new Book("fakebook2", "fakeAuth2", 2, "fakeDate2");
+var fakebook3 = new Book("fakebook3", "fakeAuth2", 2, "fakeDate2");
--- js/engine.js
+++ js/engine.js
@@ -2,32 +2,91 @@ function Library(){
   this.bookShelf = new Array();
 };
 
-
+//grabs fist full of hair
 
 
 Library.prototype.addBook = function (book){
+  //rips hair out
   var bookBool;
   function bookChk(b){
     return b === book;
-  }
+  };
+  // not even sure if this results in a bool but it works
   bookBool = gLibrary.bookShelf.find(bookChk);
-  //console.log(bookBool);
   console.log("bookchck started");
   if (!bookBool){
     gLibrary.bookShelf.push(book);
-    console.log("bookchk true, book pushed");
+    console.log("bookchk true, " + book.title + " pushed."); // kinda ghetto, book.title only works because it was pushed. probably not optimal, should correct in the future.
     return true;
   } else {
-    console.log("bookchk false, book not pushed");
+    console.log("bookchk false, book not pushed.");
     return false;
+  }
+  //invests in bosley
+  //waits for hair to grow back
+};
+
+// syntatical book-b-gone
+Library.prototype.removeBookByTitle = function (title){
+  var index = gLibrary.bookShelf.findIndex(function(b){
+    console.log("rmvBbyTtl mini function ran on: " + title);
+    return b.title === title;
+  });
+  if (index === -1){
+    console.log(title + " was not found. index: " + index);
+    return false;
+  } else {
+    gLibrary.bookShelf.splice(index,1);
+    console.log(title + " removed. index: " + index);
+    return true;
   }
 };
 
-Library.prototype.removeBookByTitle = function (title){
 
+Library.prototype.removeBookByAuthor = function (authorName){
+  var index;
+  var booksRemoved = 0;
+
+  function findAuth(){
+    index = gLibrary.bookShelf.findIndex(function(b){
+      // console.log("rmvBbyAuth mini function ran on: " + authorName);
+      return b.author === authorName;
+    });
+  };
+  findAuth();
+  if (index === -1){
+    console.log(authorName + " was not found. index: " + index);
+    return false;
+  } else {
+    while (index !== -1){
+      gLibrary.bookShelf.splice(index,1);
+      booksRemoved ++;
+      // console.log("index: " + index);
+      findAuth();
+    }
+    console.log(booksRemoved + " Book(s) by " +authorName+ " Removed.");
+    return true;
+  }
 };
 
+Library.prototype.getRandomBook = function (){
+  
+};
 
+//VVVVV takes a fraction of a second to load VVVVV ====> debuging HELL!
 document.addEventListener("DOMContentLoaded", function(e){
   window.gLibrary = new Library();
 });
+
+
+
+
+
+//vvvvv for testing only vvvvvv
+setTimeout(function(){
+
+  gLibrary.addBook(fakebook1);
+  gLibrary.addBook(ringworld);
+  gLibrary.addBook(fakebook2);
+  gLibrary.addBook(fakebook3);
+},500);

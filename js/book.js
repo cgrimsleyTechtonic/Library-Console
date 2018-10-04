d22e5c5480ab9a5ad57c63f6de425c8e2e157a54
Cody Grimsley <cody.grimsley@techtonic.com>
2018-10-04 11:36:53 -0600

    finished with main assignment, currently working on extra credit stuff. Sorry i forgot i was supposed to be pushing this stuff.

--- js/book.js
+++ js/book.js
@@ -5,13 +5,38 @@ function Book(title, author, numPages, pubDate){
   this.pubDate = pubDate;
 };
 
-Book.prototype.editBook = function(oBook){
 
+//Purpose: Takes in an object that can have some or all of the following
+// fields {title:”string” , author:”string”, numberOfPages: number} and edits the Book with the values provided
+//Return: returns the edited book object
+
+Book.prototype.editBook = function(oBook){
+  var tTitle = oBook.title;
+  var tAuth = oBook.author;
+  var tNum = oBook.numberOfPages;
+  if (typeof(tTitle) === "string" && typeof(tAuth) === "string" && typeof(tNum) === "number"){
+    this.title = tTitle;
+    this.author = tAuth;
+    this.numPages = tNum;
+  } else {console.log("Invalid value type, check that title and author are strings and page number is a number.")}
+  return this;
 };
 
+// Book.prototype.testfx = function(){
+//   var testStr = "test";
+//   var testNum = 0;
+//
+//   testStr = 0;
+//   testNum = "test";
+//   console.log("testStr: " + testStr + "\n" + "testNum: " + testNum);
+//   console.log(this.title);
+//   this.title = "editedBook";
+//   console.log(this.title);
+// }
 
-// vvvvvvvv preloaded book for testing vvvvvvv
-var fakebook1 = new Book("fakebook1", "fakeAuth1", 2, "fakeDate1");
-var ringworld = new Book("RingWorld", "Larry Niven", 342, "October 1980");
-var fakebook2 = new Book("fakebook2", "fakeAuth2", 2, "fakeDate2");
-var fakebook3 = new Book("fakebook3", "fakeAuth2", 2, "fakeDate2");
+
+// vvvvvvvv preloaded books for testing vvvvvvv
+var fakebook1 = new Book("fakebook1", "fakeAuth1", 1, new Date("September 1, 1979"));
+var ringworld = new Book("RingWorld", "Larry Niven", 342, new Date("October 1, 1980"));
+var fakebook2 = new Book("fakebook2", "fakeAuth2", 2, new Date("November 1, 1981"));
+var fakebook3 = new Book("fakebook3", "fakeAuth2", 3, new Date("December 1, 1982"));
--- js/engine.js
+++ js/engine.js
@@ -12,10 +12,10 @@ Library.prototype.addBook = function (book){
     return b === book;
   };
   // not even sure if this results in a bool but it works
-  bookBool = gLibrary.bookShelf.find(bookChk);
-  console.log("bookchck started");
+  bookBool = this.bookShelf.find(bookChk);
+  // console.log("bookchck started");
   if (!bookBool){
-    gLibrary.bookShelf.push(book);
+    this.bookShelf.push(book);
     console.log("bookchk true, " + book.title + " pushed."); // kinda ghetto, book.title only works because it was pushed. probably not optimal, should correct in the future.
     return true;
   } else {
@@ -24,44 +24,52 @@ Library.prototype.addBook = function (book){
   }
   //invests in bosley
   //waits for hair to grow back
+  //      #profit
 };
 
 // syntatical book-b-gone
 Library.prototype.removeBookByTitle = function (title){
-  var index = gLibrary.bookShelf.findIndex(function(b){
-    console.log("rmvBbyTtl mini function ran on: " + title);
+  var index = this.bookShelf.findIndex(function(b){
+    // console.log("rmvBbyTtl mini function ran on: " + title);
     return b.title === title;
   });
   if (index === -1){
     console.log(title + " was not found. index: " + index);
     return false;
   } else {
-    gLibrary.bookShelf.splice(index,1);
+    this.bookShelf.splice(index,1);
     console.log(title + " removed. index: " + index);
     return true;
   }
 };
 
 
+
 Library.prototype.removeBookByAuthor = function (authorName){
   var index;
   var booksRemoved = 0;
-
+  //VVV hacky way VVV used to correct context for the findauth fx
+  var _self = this;
+// find auth function
   function findAuth(){
-    index = gLibrary.bookShelf.findIndex(function(b){
+    index = _self.bookShelf.findIndex(function(b){
       // console.log("rmvBbyAuth mini function ran on: " + authorName);
       return b.author === authorName;
     });
   };
+  // called auth fx for first time
   findAuth();
+  // did the logic backwards on this, really more a style pref than anything.
   if (index === -1){
     console.log(authorName + " was not found. index: " + index);
     return false;
   } else {
+    //while loop to check for multiple instances of same auth
     while (index !== -1){
-      gLibrary.bookShelf.splice(index,1);
+      this.bookShelf.splice(index,1);
       booksRemoved ++;
       // console.log("index: " + index);
+      //re call auth fx to check for other instances by the same auth
       findAuth();
     }
     console.log(booksRemoved + " Book(s) by " +authorName+ " Removed.");
@@ -70,23 +78,121 @@ Library.prototype.removeBookByAuthor = function (authorName){
 };
 
 Library.prototype.getRandomBook = function (){
-  
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  var bookCnt = this.bookShelf.length;
+  // console.log("book count: " + bookCnt);
+  if (bookCnt > 1){
+    var bookLottery = getRandomInt(bookCnt);
+    // console.log("Book Lottery winnner: " + bookLottery);
+    return this.bookShelf[bookLottery];
+  } else{
+    return null;
+  }
 };
+
+
+//retrun all books that completely or partially match the string title passed into the function (indexof or regexp would help here)
+//returns an array of book objects if you find books with matching titles, empty array if no books are found.
+Library.prototype.getBookByTitle = function (title){
+  // dropped /g for now, it searches globally (finds all matches insteadof 1) but im iterating with a loop so may not need it.
+  var str = new RegExp(title,"i");
+  // console.log("var: " + str);
+  var tempArray = [];
+
+  for (var i=0; i < this.bookShelf.length; i++ ){
+    var arrValueToSearch = this.bookShelf[i].title;
+    // console.log(arrValueToSearch);
+    // console.log(str.test(arrValueToSearch));
+    if (str.test(arrValueToSearch)){
+      tempArray.push(arrValueToSearch);
+      // console.log("ran if on "+ arrValueToSearch);
+    }
+  }
+  return tempArray;
+};
+
+//  finds all books where the authors name partially or completely matches the authorName arg passed to the fx
+// returns an array of books if you find books with matching authors, empty array if no books match
+Library.prototype.getBookByAuthor = function (authorName){
+  var str = new RegExp(authorName,"i");
+  var tempArray = [];
+  // #winning #tigerblood
+  for (var i=0; i < this.bookShelf.length; i++ ){
+    var arrValueToSearch = this.bookShelf[i].author;
+    var arrValueToReturn = this.bookShelf[i].title;
+    if (str.test(arrValueToSearch)){
+      tempArray.push(arrValueToReturn);
+    }
+  }
+  return tempArray;
+};
+
+//Takes multiple books, in the form of an array of book objects, and adds the objects to your books array
+//return number of books successfully added, 0 if no books were added
+Library.prototype.addBooks = function (books){
+  var booksAdded = 0;
+  for (var i = 0; i < arguments.length; i++){
+    if (this.addBook(arguments[i])){
+      booksAdded ++;
+    }
+  }
+  console.log("Books Added: " + booksAdded);
+  return booksAdded;
+};
+
+//Find the distinct authors’ (single instance of each author) names from all books in your library
+//array of strings the names of all distinct authors, empty array if no books exist or if no authors exist
+Library.prototype.getAuthors = function (){
+  var tempArray = [];
+  var i;
+  for (i = 0; i < this.bookShelf.length; i++){
+    var tempAuth = this.bookShelf[i].author;
+    if (!tempArray.some(function(a){return a === tempAuth})){
+      tempArray.push(tempAuth);
+    }
+  };
+  return tempArray;
+};
+
+//Retrieves a random author name from your books collection
+//Return: string author name, null if no books exist
+Library.prototype.getRandomAuthorName = function (){
+  var bookHolster = null;
+  var bookAuth;
+  // console.log(bookAuth);
+  if (this.bookShelf.length > 0){
+    bookHolster = this.getRandomBook();
+    bookAuth = bookHolster.author;
+    return bookAuth;
+  } else
+  {
+    return bookHolster;
+  }
+};
+
+
+// VVVVVVVVVVVVVVVVVVVVVVVVVVVVV some custome stuff VVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+// dont run this, its just for  (unless you like to bork stuff)
+Library.prototype.nukeLibrary = function (){
+  var r = confirm("Are you sure you want to Nuke the entire Library?");
+  if (r === true){
+    this.bookShelf = [];
+    alert("You Borked It!");
+  } else { alert("Disaster Averted!");}
+  return console.log("You rand a command that you shouldn't have.");
+};
+Library.prototype.fireAutoLoad = function (){
+  this.addBooks(ringworld, fakebook2, fakebook3, fakebook1);
+  return console.log("autoload success");
+};
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^some custome stuff ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
 
 //VVVVV takes a fraction of a second to load VVVVV ====> debuging HELL!
 document.addEventListener("DOMContentLoaded", function(e){
   window.gLibrary = new Library();
 });
-
-
-
-
-
-//vvvvv for testing only vvvvvv
-setTimeout(function(){
-
-  gLibrary.addBook(fakebook1);
-  gLibrary.addBook(ringworld);
-  gLibrary.addBook(fakebook2);
-  gLibrary.addBook(fakebook3);
-},500);
